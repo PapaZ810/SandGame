2a160c18d0fc6dc420f931675348848cae12dae5
Zac <zombiezac@att.net>
2019-04-11 13:12:29 -0600

    added sand fuctionality

--- src/SandLab.java
+++ src/SandLab.java
@@ -7,6 +7,7 @@ public class SandLab
   //add constants for particle types here
   public static final int EMPTY = 0;
   public static final int METAL = 1;
+  public static final int SAND = 2;
   
   //do not add any more fields below
   private int[][] grid;
@@ -23,10 +24,11 @@ public class SandLab
     String[] names;
     // Change this value to add more buttons
     //Step 4,6
-    names = new String[2];
+    names = new String[3];
     // Each value needs a name for the button
     names[EMPTY] = "Empty";
     names[METAL] = "Metal";
+    names[SAND] = "Sand";
     
     //1. Add code to initialize the data member grid with same dimensions
     this.grid = new int[numRows][numCols];
@@ -51,9 +53,17 @@ public class SandLab
 	  {
 		  for(int col = 0; col < grid[0].length; col++)
 		  {
-			  if(grid[row][col] == 0)
+			  if(grid[row][col] == EMPTY)
 			  {
-				  setColor(row, col, Color.BLACK);
+				  display.setColor(row, col, Color.BLACK);
+			  }
+			  else if(grid[row][col] == METAL)
+			  {
+				  display.setColor(row, col, Color.GRAY);
+			  }
+			  else if(grid[row][col] == SAND)
+			  {
+				  display.setColor(row, col, Color.YELLOW);
 			  }
 		  }
 	  }
@@ -67,10 +77,22 @@ public class SandLab
     //Remember, you need to access both row and column to specify a spot in the array
     //The scalar refers to how big the value could be
     //int someRandom = (int) (Math.random() * scalar)
-    //remember that you need to watch for the edges of the array
-    
-    
-  }
+    //remember that you need to watch for the edges of the array    
+	  int rowScalar = grid.length;
+	  int colScalar = grid[0].length;
+	  int rowNum = (int) Math.random() * rowScalar;
+	  int colNum = (int) Math.random() * colScalar;
+	  
+	  
+	  if(grid[rowNum][colNum] == SAND && rowNum + 1 != rowScalar)
+		{
+		  if(grid[rowNum + 1][colNum] == EMPTY)
+		  {
+			grid[rowNum + 1][colNum] = SAND;
+			grid[rowNum][colNum] = EMPTY;
+		  }
+		}
+	}
   
   //do not modify this method!
   public void run()
